8be4cab6eba145da199a88f336ac72053465d673
Bruno Henrique Machado <brunohenrique.t17@gmail.com>
2016-10-06 17:34:03 -0300

    Melhoras substanciais na interface

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var money = 100000000000;
+var money = 100000;
 
 function dollarClick(number){
     money = money + number;
@@ -17,39 +17,39 @@ var redbullmodifier = 1;
 var globalmodifier;
 
 function buyIntern(){
-    var internCost = Math.floor(10 * Math.pow(1.1,interns));     //works out the cost of this cursor
-    if(money >= internCost){                                   //checks that the player can afford the cursor
-        interns = interns + 1;                                   //increases number of interns
-    	money = money - internCost;                          //removes the money spent
-        document.getElementById('interns').innerHTML = interns;  //updates the number of interns for the user
-        document.getElementById('money').innerHTML = money;  //updates the number of money for the user
+    var internCost = Math.floor(10 * Math.pow(1.1,interns));
+    if(money >= internCost){
+        interns = interns + 1;
+    	money = money - internCost;
+        document.getElementById('interns').innerHTML = interns;
+        document.getElementById('money').innerHTML = money;
     }
-    var nextCost = Math.floor(10 * Math.pow(1.1,interns));       //works out the cost of the next cursor
-    document.getElementById('internCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    var nextCost = Math.floor(10 * Math.pow(1.1,interns));
+    document.getElementById('internCost').innerHTML = nextCost;
 }
 
 function buyGeek(){
-    var geekCost = Math.floor(100 * Math.pow(1.1,geeks));     //works out the cost of this cursor
-    if(money >= geekCost){                                   //checks that the player can afford the cursor
-        geeks = geeks + 1;                                   //increases number of geeks
-    	money = money - geekCost;                          //removes the money spent
-        document.getElementById('geeks').innerHTML = geeks;  //updates the number of geeks for the user
-        document.getElementById('money').innerHTML = money;  //updates the number of money for the user
+    var geekCost = Math.floor(100 * Math.pow(1.1,geeks));
+    if(money >= geekCost){
+        geeks = geeks + 1;
+    	money = money - geekCost;
+        document.getElementById('geeks').innerHTML = geeks;
+        document.getElementById('money').innerHTML = money;
     }
-    var nextCost = Math.floor(100 * Math.pow(1.1,geeks));       //works out the cost of the next cursor
-    document.getElementById('geekCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    var nextCost = Math.floor(100 * Math.pow(1.1,geeks));
+    document.getElementById('geekCost').innerHTML = nextCost;
 }
 
 function buyManish(){
-    var manishCost = Math.floor(1000 * Math.pow(1.1,manishes));     //works out the cost of this cursor
-    if(money >= manishCost){                                   //checks that the player can afford the cursor
-        manishes = manishes + 1;                                   //increases number of manishes
-    	money = money - manishCost;                          //removes the money spent
-        document.getElementById('manishes').innerHTML = manishes;  //updates the number of manishes for the user
-        document.getElementById('money').innerHTML = money;  //updates the number of money for the user
+    var manishCost = Math.floor(1000 * Math.pow(1.1,manishes));
+    if(money >= manishCost){
+        manishes = manishes + 1;
+    	money = money - manishCost;
+        document.getElementById('manishes').innerHTML = manishes;
+        document.getElementById('money').innerHTML = money;
     }
-    var nextCost = Math.floor(1000 * Math.pow(1.1,manishes));       //works out the cost of the next cursor
-    document.getElementById('manishCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    var nextCost = Math.floor(1000 * Math.pow(1.1,manishes));
+    document.getElementById('manishCost').innerHTML = nextCost;
 }
 
 function buyCoffeeMachine(){
@@ -57,8 +57,9 @@ function buyCoffeeMachine(){
     	money = money - 100;
     	coffeetime = 10;
     	coffeemodifier = 2;
-        document.getElementById('money').innerHTML = money;  //updates the number of money for the user
-    	document.getElementById('coffee').innerHTML = "Activated";
+        document.getElementById('money').innerHTML = money;
+        document.getElementById("coffee").innerHTML = "Enabled"
+        document.getElementById("coffee").className="badge progress-bar-success"
     }
 }
 
@@ -67,8 +68,9 @@ function buyRedBull(){
     	money = money - 1000;
     	redbulltime = 10;
     	redbullmodifier = 4;
-        document.getElementById('money').innerHTML = money;  //updates the number of money for the user
-    	document.getElementById('redbull').innerHTML = "Full";
+        document.getElementById('money').innerHTML = money;
+        document.getElementById("redbull").innerHTML = "Full"
+        document.getElementById("redbull").className="badge progress-bar-success"
     }
 }
 
@@ -77,13 +79,15 @@ window.setInterval(function(){
 	if(coffeetime>0){
 		coffeetime = coffeetime - 1;
 	}else{
-		document.getElementById('coffee').innerHTML = "Deactivated";
+        document.getElementById("coffee").innerHTML = "Disabled"
+        document.getElementById("coffee").className="badge progress-bar-danger"
 		coffeemodifier = 1;
 	}
 	if(redbulltime>0){
 		redbulltime = redbulltime - 1;
 	}else{
-		document.getElementById('redbull').innerHTML = "Empty";
+		document.getElementById("redbull").innerHTML = "Empty"
+        document.getElementById("redbull").className="badge progress-bar-danger"
 		redbullmodifier = 1;
 	}
 	
