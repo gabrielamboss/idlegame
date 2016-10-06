d776ae61f68d2838f357e2fb36d3392311151c30
Bruno Henrique Machado <brunohenrique.t17@gmail.com>
2016-10-05 23:54:07 -0300

    Geek, Manish, Coffee, Red Bull

--- main.js
+++ main.js
@@ -1,26 +1,93 @@
-var cookies = 0;
+var money = 100000000000;
 
-function cookieClick(number){
-    cookies = cookies + number;
-    document.getElementById("cookies").innerHTML = cookies;
-};
+function dollarClick(number){
+    money = money + number;
+    document.getElementById("money").innerHTML = money;
+}
 
-var cursors = 0;
+var interns = 0;
+var geeks = 0;
+var manishes = 0;
 
-function buyCursor(){
-    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
-    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
-        cursors = cursors + 1;                                   //increases number of cursors
-    	cookies = cookies - cursorCost;                          //removes the cookies spent
-        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
-        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
-    };
-    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
-    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
-};
+var coffeetime = 0;
+var redbulltime = 0;
+
+var coffeemodifier = 1;
+var redbullmodifier = 1;
+var globalmodifier;
+
+function buyIntern(){
+    var internCost = Math.floor(10 * Math.pow(1.1,interns));     //works out the cost of this cursor
+    if(money >= internCost){                                   //checks that the player can afford the cursor
+        interns = interns + 1;                                   //increases number of interns
+    	money = money - internCost;                          //removes the money spent
+        document.getElementById('interns').innerHTML = interns;  //updates the number of interns for the user
+        document.getElementById('money').innerHTML = money;  //updates the number of money for the user
+    }
+    var nextCost = Math.floor(10 * Math.pow(1.1,interns));       //works out the cost of the next cursor
+    document.getElementById('internCost').innerHTML = nextCost;  //updates the cursor cost for the user
+}
+
+function buyGeek(){
+    var geekCost = Math.floor(100 * Math.pow(1.1,geeks));     //works out the cost of this cursor
+    if(money >= geekCost){                                   //checks that the player can afford the cursor
+        geeks = geeks + 1;                                   //increases number of geeks
+    	money = money - geekCost;                          //removes the money spent
+        document.getElementById('geeks').innerHTML = geeks;  //updates the number of geeks for the user
+        document.getElementById('money').innerHTML = money;  //updates the number of money for the user
+    }
+    var nextCost = Math.floor(100 * Math.pow(1.1,geeks));       //works out the cost of the next cursor
+    document.getElementById('geekCost').innerHTML = nextCost;  //updates the cursor cost for the user
+}
+
+function buyManish(){
+    var manishCost = Math.floor(1000 * Math.pow(1.1,manishes));     //works out the cost of this cursor
+    if(money >= manishCost){                                   //checks that the player can afford the cursor
+        manishes = manishes + 1;                                   //increases number of manishes
+    	money = money - manishCost;                          //removes the money spent
+        document.getElementById('manishes').innerHTML = manishes;  //updates the number of manishes for the user
+        document.getElementById('money').innerHTML = money;  //updates the number of money for the user
+    }
+    var nextCost = Math.floor(1000 * Math.pow(1.1,manishes));       //works out the cost of the next cursor
+    document.getElementById('manishCost').innerHTML = nextCost;  //updates the cursor cost for the user
+}
+
+function buyCoffeeMachine(){
+    if(money >= 100){                                   
+    	money = money - 100;
+    	coffeetime = 10;
+    	coffeemodifier = 2;
+        document.getElementById('money').innerHTML = money;  //updates the number of money for the user
+    	document.getElementById('coffee').innerHTML = "Activated";
+    }
+}
+
+function buyRedBull(){
+    if(money >= 1000){                                   
+    	money = money - 1000;
+    	redbulltime = 10;
+    	redbullmodifier = 4;
+        document.getElementById('money').innerHTML = money;  //updates the number of money for the user
+    	document.getElementById('redbull').innerHTML = "Full";
+    }
+}
 
 window.setInterval(function(){
+
+	if(coffeetime>0){
+		coffeetime = coffeetime - 1;
+	}else{
+		document.getElementById('coffee').innerHTML = "Deactivated";
+		coffeemodifier = 1;
+	}
+	if(redbulltime>0){
+		redbulltime = redbulltime - 1;
+	}else{
+		document.getElementById('redbull').innerHTML = "Empty";
+		redbullmodifier = 1;
+	}
 	
-	cookieClick(cursors);
-	
+	globalmodifier = Math.max(coffeemodifier*redbullmodifier, 1);
+	dollarClick(globalmodifier*interns + globalmodifier*10*geeks + globalmodifier*100*manishes);	
+
 }, 1000);
